1ea30262e264e88e730179235796aee35a8d9af8
Carl Svensson <csvenss2@gmail.com>
2026-01-21 23:12:55 -0800

    prefix is messing up when uploading from development.

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,12 +1,12 @@
-# Add a prefix to all ActiveStorage blob keys for this engine
-Rails.configuration.to_prepare do
-  ActiveStorage::Blob.class_eval do
-    before_create :add_document_archive_prefix
-
-    private
-
-    def add_document_archive_prefix
-      self.key = "document_archive-#{Rails.env}/#{self.class.generate_unique_secure_token}"
-    end
-  end
-end
+## Add a prefix to all ActiveStorage blob keys for this engine
+#Rails.configuration.to_prepare do
+#  ActiveStorage::Blob.class_eval do
+#    before_create :add_document_archive_prefix
+#
+#    private
+#
+#    def add_document_archive_prefix
+#      self.key = "document_archive-#{Rails.env}/#{self.class.generate_unique_secure_token}"
+#    end
+#  end
+#end
